51f7e45fa6b63d64b0a90100f683fef4b593076a
Francesco Ioli <francesco.ioli@polimi.it>
2023-04-16 12:25:37 +0200

    added check_os fun in build_potree.py

--- scripts/build_potree.py
+++ scripts/build_potree.py
@@ -3,6 +3,22 @@ import subprocess
 import sys
 from pathlib import Path
 
+
+def check_os() -> str:
+    if sys.platform == "win32":
+        return "win"
+    elif sys.platform == "linux":
+        return "linux"
+    elif sys.platform == "darwin":
+        return "macos"
+    else:
+        raise OSError("Unsupported OS")
+
+
+def get_potree_converter_path(os: str) -> Path:
+    return Path(__file__).parent.parent / "potree"
+
+
 if __name__ == "__main__":
 
     print("Done.")
