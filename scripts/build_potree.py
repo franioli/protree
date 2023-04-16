52dda99a8bc0fb8ac6bb88d26e7003f7e78b48d7
Francesco Ioli <francesco.ioli@polimi.it>
2023-04-16 12:38:09 +0200

    updated build_potree.py

--- scripts/build_potree.py
+++ scripts/build_potree.py
@@ -1,7 +1,12 @@
 import os
+import parser
 import subprocess
 import sys
 from pathlib import Path
+
+
+def parse_command_line_args() -> dict:
+    pass
 
 
 def check_os() -> str:
@@ -15,10 +20,25 @@ def check_os() -> str:
         raise OSError("Unsupported OS")
 
 
-def get_potree_converter_path(os: str) -> Path:
-    return Path(__file__).parent.parent / "potree"
+def get_potree_converter_path() -> Path:
+    os = check_os()
+    project_root = Path(__file__).parents[1]
+    if os == "win":
+        path = project_root / "PotreeConverter.exe"
+        assert path.exists(), f"PotreeConverter.exe not found in {project_root} folder."
+    if os == "linux":
+        path = project_root / "PotreeConverter"
+        assert path.exists(), f"PotreeConverter not found in {project_root} folder."
+    if os == "macos":
+        path = project_root / "PotreeConverter"
+        assert path.exists(), f"PotreeConverter not found in {project_root} folder."
+
+    return path
 
 
 if __name__ == "__main__":
+    pc_path = get_potree_converter_path()
+
+    print("Building Potree...")
 
     print("Done.")
